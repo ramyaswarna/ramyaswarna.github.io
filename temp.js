b0bc18457d30a095524ee275826ee073d96300cc
ramyaswarna <ramyaswarna725@gmail.com>
2024-11-20 17:51:19 +0530

    sticky

---
+++ temp.js
@@ -0,0 +1,16 @@
+db.products.insertMany(
+    [
+    {
+        "name":"Product 5",
+        "desc":"This is the description of product 5",
+        "price":999,
+        "url":"https://picsum.photos/id/77/450/300"
+    },
+    {
+        "name":"Product 6",
+        "desc":"This is the description of product 6",
+        "price":1,
+        "url":"https://picsum.photos/id/77/450/300"
+    }
+]
+);
